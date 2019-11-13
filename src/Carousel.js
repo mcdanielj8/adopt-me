4897d27ad8454817dc262109ea8773925784a364
Kyle McDaniel <mcdanieljk8@gmail.com>
2019-11-12 20:47:45 -0500

    add router, carousel

---
+++ src/Carousel.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+class Carousel extends React.component {
+  state = {
+    photos: [],
+    active: 0
+  };
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600/600"];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    return { photos };
+  }
+  render() {
+    const { photos, active } = this.state;
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="Animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            //eslint-disable-next-line
+            <img
+              key={photo}
+              onClick={this.handleClick}
+              data-index={index}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
